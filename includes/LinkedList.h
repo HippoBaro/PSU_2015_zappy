45b0389b8d93c710c1cac9eb2da8f54043df1fac
pasteu_e <etienne.pasteur@epitech.eu>
2016-06-11 22:40:18 +0200

    Adding LinkedList

---
+++ includes/LinkedList.h
@@ -0,0 +1,52 @@
+/*
+** LinkedList.h for PSU_2015_zappy in /home/pasteu_e/rendu/PSU_2015_zappy
+**
+** Made by Etienne Pasteur
+** Login   <pasteu_e@epitech.net>
+**
+** Started on  Sat Jun 11 17:47:57 2016 Etienne Pasteur
+** Last update Sat Jun 11 21:34:39 2016 Etienne Pasteur
+*/
+
+#ifndef LINKED_LIST_H_
+# define LINKED_LIST_H_
+
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef enum {false, true} bool;
+
+typedef struct	s_list
+{
+  void		*data;
+  struct s_list	*next;
+  struct s_list	*prev;
+}		t_list;
+
+typedef struct	LinkedList
+{
+  t_list	*myList;
+
+  void		(*printLinkedList)(struct LinkedList *this);
+  int		(*countLinkedList)(struct LinkedList *this);
+  bool		(*addElemFront)(struct LinkedList *this, void *data);
+  bool		(*addElemEnd)(struct LinkedList *this, void *data);
+  bool		(*addElemAtPos)(struct LinkedList *this, int pos, void *data);
+  bool		(*removeElemFront)(struct LinkedList *this);
+  bool		(*removeElemEnd)(struct LinkedList *this);
+  bool		(*removeElemAtPos)(struct LinkedList *this, int pos);
+  bool		(*freeElemFront)(struct LinkedList *this, void (*freeFunc)(void *elem));
+  bool		(*freeElemEnd)(struct LinkedList *this, void (*freeFunc)(void *elem));
+  bool		(*freeElemAtPos)(struct LinkedList *this, int pos, void (*freeFunc)(void *elem));
+  bool		(*freeAll)(struct LinkedList *this, void (*freeFunc)(void *elem));
+  t_list*	(*getElementFirst)(struct LinkedList *this);
+  t_list*	(*getElementEnd)(struct LinkedList *this);
+  t_list*	(*getElementAtPos)(struct LinkedList *this, int pos);
+  t_list*	(*FirstElementFromPredicate)(struct LinkedList *this, int (*Predicate)(void *elem, void* data));
+  void		(*forEachElements)(struct LinkedList *this, void (*forEachFunc)(void *element, void *userData));
+}		LinkedList;
+
+void		LinkedListInit(LinkedList *this);
+void		LinkedListDestroy(LinkedList *this);
+
+#endif /* !LINKED_LIST_H_ */
---
+++ srcs/LinkedList.c
@@ -0,0 +1,262 @@
+/*
+** LinkedList.c for PSU_2015_zappy in /home/pasteu_e/rendu/PSU_2015_zappy
+**
+** Made by Etienne Pasteur
+** Login   <pasteu_e@epitech.net>
+**
+** Started on  Sat Jun 11 17:47:03 2016 Etienne Pasteur
+** Last update Sat Jun 11 22:37:59 2016 Etienne Pasteur
+*/
+
+#include <LinkedList.h>
+
+static void	printLinkedList(LinkedList *this);
+static int	countLinkedList(LinkedList *this);
+static bool	addElemFront(LinkedList *this, void *data);
+static bool	addElemEnd(LinkedList *this, void *data);
+static bool	addElemAtPos(LinkedList *this, int pos, void *data);
+static bool	removeElemFront(LinkedList *this);
+static bool	removeElemEnd(LinkedList *this);
+static bool	removeElemAtPos(LinkedList *this, int pos);
+static bool	freeElemFront(LinkedList *this, void (*freeFunc)(void *elem));
+static bool	freeElemEnd(LinkedList *this, void (*freeFunc)(void *elem));
+static bool	freeElemAtPos(LinkedList *this, int pos, void (*freeFunc)(void *elem));
+static bool	freeAll(LinkedList *this, void (*freeFunc)(void *elem));
+static t_list*	getElementFirst(LinkedList *this);
+static t_list*	getElementEnd(LinkedList *this);
+static t_list*	getElementAtPos(LinkedList *this, int pos);
+static t_list*	FirstElementFromPredicate(LinkedList *this, int (*Predicate)(void *elem, void* data));
+static void	forEachElements(LinkedList *this, void (*forEachFunc)(void *element, void *userData));
+
+static void	initPtrFunc(LinkedList *this)
+{
+  this->printLinkedList = &printLinkedList;
+  this->countLinkedList = &countLinkedList;
+  this->addElemFront = &addElemFront;
+  this->addElemEnd = &addElemEnd;
+  this->addElemAtPos = &addElemAtPos;
+  this->removeElemFront = &removeElemFront;
+  this->removeElemEnd = &removeElemEnd;
+  this->removeElemAtPos = &removeElemAtPos;
+  this->freeElemFront = &freeElemFront;
+  this->freeElemEnd = &freeElemEnd;
+  this->freeElemAtPos = &freeElemAtPos;
+  this->freeAll = &freeAll;
+  this->getElementFirst = &getElementFirst;
+  this->getElementEnd = &getElementEnd;
+  this->getElementAtPos = &getElementAtPos;
+  this->FirstElementFromPredicate = &FirstElementFromPredicate;
+  this->forEachElements = &forEachElements;
+}
+
+void		LinkedListInit(LinkedList *this)
+{
+  initPtrFunc(this);
+  this->myList = malloc(sizeof t_list*);
+  if (this->myList != NULL )
+    {
+      this->myList->prev = this->myList;
+      this->myList->next = this->myList;
+    }
+}
+
+void		LinkedListDestroy(LinkedList *this)
+{
+  t_list	*it;
+  t_list	*next;
+
+  it = this->myList->next;
+  while (it != this->myList)
+    {
+      next = it->next;
+      free(it);
+      it = next;
+    }
+  free(this->myList);
+  this->myList = NULL;
+}
+
+static void	printLinkedList(LinkedList *this)
+{
+  t_list	*it;
+  int		i;
+
+  i = 0;
+  it = this->myList->next;
+  while (it != this->myList)
+    {
+      it = it->next;
+      i += 1;
+    }
+  printf("Nb_elem : %d\n", i);
+}
+
+static int	countLinkedList(LinkedList *this)
+{
+  t_list	*it;
+  int		i;
+
+  i = 0;
+  it = this->myList->next;
+  while (it != this->myList)
+    {
+      it = it->next;
+      i += 1;
+    }
+  return (i);
+}
+
+static bool	addElemFront(LinkedList *this, void *data)
+{
+  t_list	*newElem;
+
+  newElem = malloc(sizeof t_list*);
+  if (newElem != NULL)
+    {
+      newElem->data = data;
+      newElem->prev = this->myList->prev;
+      newElem->next = this->myList;
+      this->myList->prev->next = newElem;
+      this->myList->prev = newElem;
+      return (true);
+    }
+  return (false);
+}
+
+static bool	addElemEnd(LinkedList *this, void *data)
+{
+  t_list	*newElem;
+
+  newElem = malloc(sizeof t_list*);
+  if (newElem != NULL)
+    {
+      newElem->data = data;
+      newElem->prev = this->myList;
+      newElem->next = this->myList->next;
+      this->myList->next->prev = newElem;
+      this->myList->next = newElem;
+      return (true);
+    }
+  return (false);
+}
+
+static bool	addElemAtPos(LinkedList *this, int pos, void *data)
+{
+  t_list	*it;
+  int		i;
+  t_list	*newElem;
+
+  i = 0;
+  it = this->myList->next;
+  while (it != this->myList)
+    {
+      if (i == pos)
+	{
+	  newElem = malloc(sizeof t_list*);
+	  if (newElem != NULL)
+	    {
+	      newElem->data = data;
+	      newElem->prev = it;
+	      newElem->next = it->next;
+	      it->next->prev = newElem;
+	      it->next = newElem;
+	      return (true);
+	    }
+	  else
+	    return (false);
+	}
+      it = it->next;
+      i += 1;
+    }
+  return (false);
+}
+
+static bool	removeElemFront(LinkedList *this)
+{
+  if (this->myList->next != this)
+    {
+      this->myList->next->prev->next = this->myList->next->next;
+      this->myList->next->next->prev = this->myList->next->prev;
+      return (true);
+    }
+  return (false);
+}
+
+static bool	removeElemEnd(LinkedList *this)
+{
+  if (this->myList->prev != this)
+    {
+      this->myList->prev->prev->next = this->myList->prev->next;
+      this->myList->prev->next->prev = this->myList->prev->prev;
+      return (true);
+    }
+  return (false);
+}
+
+static bool	removeElemAtPos(LinkedList *this, int pos)
+{
+
+}
+
+static bool	freeElemFront(LinkedList *this, void (*freeFunc)(void *elem))
+{
+
+}
+
+static bool	freeElemEnd(LinkedList *this, void (*freeFunc)(void *elem))
+{
+
+}
+
+static bool	freeElemAtPos(LinkedList *this, int pos, void (*freeFunc)(void *elem))
+{
+
+}
+
+static bool	freeAll(LinkedList *this, void (*freeFunc)(void *elem))
+{
+
+}
+
+static t_list*	getElementFirst(LinkedList *this)
+{
+  if (this->myList->next != this->myList)
+    return (this->myList->next);
+  return (NULL);
+}
+
+static t_list*	getElementEnd(LinkedList *this)
+{
+  if (this->myList->prev != this->myList)
+    return (this->myList->prev);
+  return (NULL);
+}
+
+static t_list*	getElementAtPos(LinkedList *this, int pos)
+{
+  t_list	*it;
+  int		i;
+
+  i = 0;
+  it = this->myList->next;
+  while (it != this->myList)
+    {
+      if (i == pos)
+	{
+	  return (it);
+	}
+      it = it->next;
+      i += 1;
+    }
+  return (NULL);
+}
+
+static t_list*	FirstElementFromPredicate(LinkedList *this, int (*Predicate)(void *elem, void* data))
+{
+
+}
+
+static void	forEachElements(LinkedList *this, void (*forEachFunc)(void *element, void *userData))
+{
+
+}
