37d01df578676dff2b8b5441e017b4b5b56d569b
diacon_c <diacon_c@epitech.eu>
2016-06-21 17:49:20 +0200

    Many tweaks in client

--- client-srcs/zappy.client/Class/Generic/Constantes.cs
+++ client-srcs/zappy.client/Class/Generic/Constantes.cs
@@ -9,5 +9,21 @@ namespace Zappy.Client
     public static class Constantes
     {
         public const bool IS_DEBUG = true;
+
+        /* Generic Network */
+        public const string NETWORK_REQUEST_ANSWER_OK = "ok\n";
+        public const string NETWORK_REQUEST_ANSWER_KO = "ko\n";
+
+        /* DroneManagementRequest */
+        public const string NETWORK_REQUEST_MOVE_FORWARD = "avance\n";
+        public const string NETWORK_REQUEST_TURN_LEFT = "gauche\n";
+        public const string NETWORK_REQUEST_TURN_RIGHT = "droite\n";
+        public const string NETWORK_REQUEST_DRONE_FORK = "fork\n";
+        public const string NETWORK_REQUEST_DRONE_SEE = "voir\n";
+        public const string NETWORK_REQUEST_DRONE_INVENTAIRE = "inventaire\n";
+        public const string NETWORK_REQUEST_DRONE_EXPULSE = "expulse\n";
+
+        /* BroadcastRequest */
+        public const string NETWORK_REQUEST_DRONE_BROADCAST = "broadcast ";
     }
 }
