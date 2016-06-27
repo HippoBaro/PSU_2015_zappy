27e1499a2ea83a1c6e9dff721319a214c26a6f3a
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-06-27 13:22:40 +0200

    Fixed client bug

--- Client/Client/Client/Program.cs
+++ Client/Client/Client/Program.cs
@@ -8,7 +8,6 @@ namespace Client
 		public string Team { get; private set; }
 
 		public void Configure(string[] args) {
-			try {
 				for (int i = 0; i < args.Length; i++) {
 					if (args [i] == "-h")
 						Host = args [++i];
@@ -19,10 +18,12 @@ namespace Client
 				}
 				if (string.IsNullOrWhiteSpace(Host) || string.IsNullOrWhiteSpace(Team) || Port < 1024)
 					throw new InvalidProgramException();
-			} catch (Exception ex) {
-				Console.WriteLine ("Invalid arguments");
-				Environment.Exit (0);
-			}
+		}
+
+		public Option() {
+			Host = string.Empty;
+			Port = 0;
+			Team = string.Empty;
 		}
 	}
 
@@ -31,14 +32,19 @@ namespace Client
 		public static void Main (string[] args)
 		{
 			var option = new Option();
-			option.Configure (args);
+			try {
+				option.Configure (args);
+			} catch (Exception ex) {
+				Console.WriteLine ("Invalid arguments");
+				return;
+			}
 			var net = new Network (option);
 
 			Console.WriteLine(net.ReceiveMessage(TimeSpan.FromSeconds(1)));
 			net.SendMessage(option.Team);
 			if (net.ReceiveMessage (null) == "ko") {
 				Console.WriteLine ("Server rejected client.");
-				Environment.Exit (0);
+				return;
 			}
 			while (true) {
 				Console.WriteLine ("Waiting");
